3a583dd2a765513081f5548acddb1eb18e9c7ff2
zxymike <zxymike93@163.com>
2016-10-16 23:33:08 +0800

    一个清真的 log

---
+++ utils.py
@@ -0,0 +1,12 @@
+import time
+
+def log(*args, **kwargs):
+    """
+    用 log 代替 print
+    time.time() 返回 Unix time
+    再转为常用时间格式
+    """
+    format = '%Y/%m/%d %H:%M:%S'
+    value = time.localtime(int(time.time()))
+    dt = time.strftime(format, value)
+    print(dt, *args, **kwargs)
